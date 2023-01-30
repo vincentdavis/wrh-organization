29c0e1927437d6375412de95c5ce6aecfd5eaba8
Ali Eghlima <pprolancer@gmail.com>
2023-01-30 14:26:31 +0400

    [RELEASE] 1.0.9-0.0.89

---
+++ wrh_organization/FRONTEND/wrh_organization_ui/dist/js/chunk-2d0effff.51b12d24.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0effff"],{"9b00":function(e,t,n){"use strict";n.r(t);var u=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"widget-race-results pa-2"},[n("public-event-profile")],1)},l=[],c=n("4463"),s={components:{PublicEventProfile:c["default"]},setup:function(){return{}}},i=s,r=n("2877"),a=Object(r["a"])(i,u,l,!1,null,null,null);t["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0effff.51b12d24.js.map
