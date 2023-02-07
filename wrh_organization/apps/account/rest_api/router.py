1b42d1d542429ce27194a74afe15381bebedda1f
cerberusmaster <robert.19960318@gmail.com>
2023-02-07 11:41:05 -0300

    remove apps/account

--- wrh_organization/apps/account/rest_api/router.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path, re_path
-from rest_framework import routers
-
-from apps.account.rest_api.views import SessionView, ProfileView
-
-rest_router = routers.DefaultRouter()
-rest_router.trailing_slash = "/?"  # added to support both / and slashless
-rest_router.register(r'session', SessionView, basename='session')
-rest_router.register(r'me', ProfileView, basename='profile')
--- wrh_organization/apps/account/urls.py
+++
@@ -1,8 +0,0 @@
-from .rest_api import router
-
-app_name = 'account'
-
-urlpatterns = [
-]
-
-urlpatterns += router.rest_router.urls
