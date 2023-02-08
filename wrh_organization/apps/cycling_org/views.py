46765ae25c8ace1cfe5055d2ee7e550ea82d458a
angel9015 <jc11091005@gmail.com>
2023-02-08 02:01:03 -0500

    #172 calendar feed issue

--- wrh_organization/apps/cycling_org/views.py
+++ wrh_organization/apps/cycling_org/views.py
@@ -3,12 +3,18 @@ import traceback
 from PIL import Image
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django_ckeditor_5.forms import UploadFileForm
 from django_ckeditor_5.views import storage as ck_storage
 
 from wrh_organization.helpers.utils import get_random_upload_path
+
+import datetime
+from django_ical.views import ICalFeed
+from icalendar import vCalAddress, vText
+from .models import Event
 
 
 @require_http_methods(["POST"])
@@ -27,3 +33,43 @@ def ckeditor_upload_file(request):
         file_name = fs.save(file_path, f)
         url = fs.url(file_name)
         return JsonResponse({"url": url})
+
+class EventFeed(ICalFeed):
+    """
+    A WRH Event Calender
+    """
+    product_id = '-//WRH Organization.//EN'
+    # timezone = 'UTC'
+    file_name = "event.ics"
+    title = "WRH Events"
+    description = "Public Events of WRH. Provided by https://weracehere.org/"
+
+    def items(self):
+        return Event.objects.all().order_by('-create_datetime')
+
+    def item_title(self, item):
+        return item.name
+
+    def item_description(self, item):
+        return ("http://localhost:8080" if settings.DEBUG else "https://dev2.weracehere.org" ) + "/static/vue/#/event-profile/" + str(item.id)
+
+    def item_start_datetime(self, item):
+        return item.start_date
+    
+    def item_end_datetime(self, item):
+        return item.end_date
+
+    def item_created(self, item):
+        return item.create_datetime
+
+    def item_updateddate(self, item):
+        return item.update_datetime
+
+    def item_location(self, item):
+        return item.country
+
+    def item_class(self, item):
+        return "Public"
+
+    def item_guid(self, item):
+        return str(item.id) + "@www.werachere.org"
