d174aae72391f5b0aa58c213f3c0d845315d8fc5
Ali Eghlima <pprolancer@gmail.com>
2023-03-01 17:35:28 +0400

    [RELEASE] 1.1.9-0.1.7

---
+++ wrh_organization/FRONTEND/wrh_organization_ui/dist/js/chunk-2d2217f3.d7d0e604.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2217f3"],{cb31:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"widget-org-profile"},[t("public-org-profile")],1)},l=[],r=t("387c"),u={components:{PublicOrgProfile:r["default"]},setup:function(){return{}}},i=u,o=t("2877"),s=Object(o["a"])(i,c,l,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d2217f3.d7d0e604.js.map
