2845dec42e57e550dc070e5818789cdc34bafd3a
Vincent Davis <vincent@heteroskedastic.net>
2023-03-30 06:39:29 -0600

    Add login/out url settings, commented out

--- wrh_organization/wrh_organization/settings/base.py
+++ wrh_organization/wrh_organization/settings/base.py
@@ -157,6 +157,8 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 50 * 1024 * 1024  # 50 MB
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 VUE_STATIC_INDEX = '/static/vue/index.html'
 LOGIN_URL = f'{VUE_STATIC_INDEX}#/dashboard/home'
+# LOGIN_REDIRECT_URL = "events-dv"
+# LOGOUT_REDIRECT_URL = "events-dv"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "collected_static"
