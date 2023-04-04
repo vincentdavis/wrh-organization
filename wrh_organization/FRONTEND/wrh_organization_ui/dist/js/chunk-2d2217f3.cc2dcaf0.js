4827869f040c844bdaa76ec034a72ff30eb59a07
Vincent Davis <vincent@vincentdavis.net>
2023-04-04 09:03:47 -0600

    [RELEASE] 1.1.55-0.1.16

---
+++ wrh_organization/FRONTEND/wrh_organization_ui/dist/js/chunk-2d2217f3.cc2dcaf0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2217f3"],{cb31:function(e,t,n){"use strict";n.r(t);var l=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"widget-org-profile"},[n("public-org-profile")],1)},r=[],c=n("387c"),u={components:{PublicOrgProfile:c["default"]},setup(){return{}}},i=u,o=n("2877"),s=Object(o["a"])(i,l,r,!1,null,null,null);t["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d2217f3.cc2dcaf0.js.map
