70e3a65fc68a1500b18f4e1ae9f0aa8a89a9405c
Vincent Davis <vincent@heteroskedastic.net>
2023-04-10 06:40:03 -0600

    Apply code formatting, fix import

--- wrh_organization/apps/cycling_org/views_clubs.py
+++ wrh_organization/apps/cycling_org/views_clubs.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, render
 
-from .models import Organization
 from .forms import JoinClubForm
+from .models import Organization
+
 
 def join_club(request, pk=None):
     if request.method == 'POST':
